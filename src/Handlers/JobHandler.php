2aebb0e481b3e4b5da47395002a9e7533e487fed
PordakSergey <pordakk@gmail.com>
2026-01-30 17:39:27 +0300

    add job serializer

--- src/Handlers/JobHandler.php
+++ src/Handlers/JobHandler.php
@@ -2,19 +2,24 @@
 
 namespace Rr\Bundle\Workers\Handlers;
 
+use Psr\Log\LoggerInterface;
 use Rr\Bundle\Workers\Contracts\Handlers\JobHandlerInterface;
-use Rr\Bundle\Workers\Contracts\Jobs\JobCommandInterface;
 use Spiral\RoadRunner\Jobs\Task\ReceivedTaskInterface;
 use Symfony\Component\Messenger\Exception\ExceptionInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 final class JobHandler implements JobHandlerInterface
 {
     /**
+     * @param SerializerInterface $serializer
      * @param MessageBusInterface $messageBus
+     * @param LoggerInterface $logger
      */
     public function __construct(
+        protected SerializerInterface $serializer,
         protected MessageBusInterface $messageBus,
+        protected LoggerInterface       $logger,
     )
     {
     }
@@ -22,15 +27,15 @@ final class JobHandler implements JobHandlerInterface
     /**
      * @param ReceivedTaskInterface $task
      * @return void
-     * @throws ExceptionInterface
      */
     public function handle(ReceivedTaskInterface $task): void
     {
-        $taskClass = $task->getName();
-        $command = new $taskClass();
-        if ($command instanceof JobCommandInterface) {
-            $command->setPayload(json_decode($task->getPayload(), true));
+        try {
+            $taskClass = $task->getName();
+            $command = $this->serializer->deserialize($task->getPayload(), $taskClass, 'json');
             $this->messageBus->dispatch($command);
+        } catch (ExceptionInterface|\Symfony\Component\Serializer\Exception\ExceptionInterface $e) {
+            $this->logger->error('Failed dispatch job', ['exception' => $e->getMessage()]);
         }
     }
 }
--- src/Jobs/JobsDispatcher.php
+++ src/Jobs/JobsDispatcher.php
@@ -9,38 +9,41 @@ use Spiral\RoadRunner\Environment;
 use Spiral\RoadRunner\Jobs\Exception\JobsException;
 use Spiral\RoadRunner\Jobs\Jobs;
 use Spiral\RoadRunner\Jobs\Options;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class JobsDispatcher
 {
     /**
      * @param RPCFactory $rpcFactory
      * @param LoggerInterface $logger
+     * @param SerializerInterface $serializer
      */
     public function __construct(
-        private RPCFactory      $rpcFactory,
-        private LoggerInterface $logger,
-
+        protected RPCFactory          $rpcFactory,
+        protected LoggerInterface     $logger,
+        protected SerializerInterface $serializer,
     )
     {
     }
 
     /**
+     * @param object $command
      * @param string $queueName
-     * @param JobCommandInterface $command
      * @param Options $options
      * @return string|null
      */
-    public function dispatch(string $queueName, JobCommandInterface $command, Options $options = new Options()): ?string
+    public function dispatch(object $command, string $queueName = 'job', Options $options = new Options()): ?string
     {
         try {
             $jobs = new Jobs($this->rpcFactory::fromEnvironment(Environment::fromGlobals()));
             $queue = $jobs->connect($queueName);
-            $task = $queue->create($command::class, json_encode($command->getPayload()), $options);
+            $task = $queue->create($command::class, $this->serializer->serialize($command, 'json'), $options);
             $sendTask = $queue->dispatch($task);
 
             return $sendTask->getId();
-        } catch (JobsException $e) {
-            $this->logger->error('Error job: ' . $command::class . ' payload: ' . json_encode($command->getPayload()) . ' error: ' . $e->getMessage());
+        } catch (JobsException|ExceptionInterface $e) {
+            $this->logger->error('Error job: ' . $command::class . ' error: ' . $e->getMessage());
         }
         return null;
     }
