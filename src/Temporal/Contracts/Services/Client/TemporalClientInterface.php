68d303601b5fc25ac6c9aed6cb5f2e122dbeaa44
PordakSergey <pordakk@gmail.com>
2026-02-16 19:20:58 +0300

    new implementation workflow/activity register

--- src/Temporal/Contracts/Services/Client/TemporalClientInterface.php
+++ src/Temporal/Contracts/Services/Client/TemporalClientInterface.php
@@ -10,4 +10,10 @@ interface TemporalClientInterface
      * @return WorkflowClient
      */
     public function getClient() : WorkflowClient;
+
+    /**
+     * @param object $command
+     * @return string
+     */
+    public function dispatch(object $command) : string;
 }
